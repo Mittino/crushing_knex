1a2f9cb253d36ce0eabc3f1cb303afcbe0a975e3
Anna Mefford <annamefford@gmail.com>
2016-11-18 11:41:28 -0700

    migration file

---
+++ migration.sql
@@ -0,0 +1,17 @@
+CREATE TABLE manufacturer{
+  id serial PRIMARY KEY,
+  name text
+}
+
+CREATE TABLE lines{
+  id serial PRIMARY KEY,
+  line_name text,
+  mfctr_id integer NOT NULL REFERENCES manufacturer ON DELETE CASCADE
+}
+
+CREATE TABLE stylenumbers{
+  id serial PRIMARY KEY,
+  style_id varchar(20),
+  line_id integer NOT NULL REFERENCES lines ON DELETE CASCADE,
+  mfctr_id integer NOT NULL REFERENCES manufacturer ON DELETE CASCADE
+}
